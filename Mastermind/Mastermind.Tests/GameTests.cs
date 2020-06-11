ebc418771f0e1a7bd67864273e1c0603c569e553
Dan <drasmussen49@gmail.com>
2020-06-11 13:30:39 -0400

    Created method to put randomized values in MasterArray in Game.cs, wrote a test in GameTests to check method didn't contain zeroes after method is ran

--- Mastermind/Mastermind.Tests/GameTests.cs
+++ Mastermind/Mastermind.Tests/GameTests.cs
@@ -6,13 +6,16 @@ namespace Mastermind.Tests
     public class GameTests
     {
         [Fact]
-        public void Test1()
+        public void CheckMasterArrayValuesNotZero()
         {
             //arrange
-
+            Game game = new Game();
+            
             //act
+            game.SetMasterArray();
 
             //assert
+            Assert.DoesNotContain(0, game.masterArray);
         }
     }
 }
--- Mastermind/Mastermind/Game.cs
+++ Mastermind/Mastermind/Game.cs
@@ -6,6 +6,27 @@ namespace Mastermind
 {
     public class Game
     {
+        public int[] masterArray = new int[4];
 
+        public int[] MasterArray { get; set; }
+        public int GuessCounter { get; set; }
+
+        public Game()
+        {
+            MasterArray = masterArray;
+        }
+
+        public void SetMasterArray()
+        {
+            Random rnd = new Random();
+            int firstNum = rnd.Next(1, 7);
+            int secondNum = rnd.Next(1, 7);
+            int thirdNum = rnd.Next(1, 7);
+            int fourthNum = rnd.Next(1, 7);
+            MasterArray[0] = firstNum;
+            MasterArray[1] = secondNum;
+            MasterArray[2] = thirdNum;
+            MasterArray[3] = fourthNum;
+        }
     }
 }
